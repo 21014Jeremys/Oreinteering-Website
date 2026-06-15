9f988d5fcc23110a96aaa03fcbf564167aa98844
21014Jeremys <21014@jpc.school.nz>
2026-06-16 09:28:24 +1200

    Added Hook image

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,33 @@
+console.log("Hey, you are reading this!");
+
+let list_data = [];
+const demon_button = document.getElementById("demon_button");
+console.log(demon_button);
+
+const style = document.styleSheets[0];
+console.log(style.cssRules[0]);
+
+const iframe = document.getElementById("header");
+iframe.onload = function(){
+    const iframeDoc = iframe.contentWindow.document;
+    
+    const logo = iframeDoc.getElementById("website_logo");
+    const header = iframeDoc.getElementById("header");
+
+    if (demon_button != null){
+        demon_button.onclick = function(){
+            window.alert("You have doomed yourself!");
+            logo.src = "../images/logo_with_demon.svg";
+            header.style.backgroundColor = "#000000";
+            console.log(logo.src);
+        };
+    }
+};
 
 
 
-console.log("Is this working?");
-console.log("Attempt two!");
 
-let list_data = []
-
-
-const sub_button = document.getElementById("submit_button")
+const sub_button = document.getElementById("submit_button");
 
 if (sub_button != null){
     sub_button.onclick = function(){
@@ -16,7 +36,7 @@ if (sub_button != null){
         list_data.push(document.getElementById("subject_input").value);
         list_data.push(document.getElementById("message_input").value);
         console.log(list_data);
-    }
+    };
 } else {
     console.log("No button found :(");
 }
